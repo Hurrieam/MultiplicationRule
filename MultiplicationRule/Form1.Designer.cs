1c7f89c55d1e7625f068312bb640144aa16106a6
Hurrieam <77335318+Hurrieam@users.noreply.github.com>
2021-01-13 00:49:36 +0800

    Add files via upload

---
+++ MultiplicationRule/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace MultiplicationRule
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num_1_lbl = new System.Windows.Forms.Label();
+            this.IsMultipliedBy = new System.Windows.Forms.Label();
+            this.num_2_lbl = new System.Windows.Forms.Label();
+            this.NewWork_btn = new System.Windows.Forms.Button();
+            this.input_textbox = new System.Windows.Forms.TextBox();
+            this.Submit_btn = new System.Windows.Forms.Button();
+            this.WorkNum_lbl = new System.Windows.Forms.Label();
+            this.one_btn = new System.Windows.Forms.Button();
+            this.two_btn = new System.Windows.Forms.Button();
+            this.three_btn = new System.Windows.Forms.Button();
+            this.four_btn = new System.Windows.Forms.Button();
+            this.five_btn = new System.Windows.Forms.Button();
+            this.six_btn = new System.Windows.Forms.Button();
+            this.seven_btn = new System.Windows.Forms.Button();
+            this.eight_btn = new System.Windows.Forms.Button();
+            this.nine_btn = new System.Windows.Forms.Button();
+            this.zero_btn = new System.Windows.Forms.Button();
+            this.Clear_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // num_1_lbl
+            // 
+            this.num_1_lbl.AutoSize = true;
+            this.num_1_lbl.Font = new System.Drawing.Font("微软雅黑", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.num_1_lbl.Location = new System.Drawing.Point(84, 9);
+            this.num_1_lbl.Name = "num_1_lbl";
+            this.num_1_lbl.Size = new System.Drawing.Size(67, 75);
+            this.num_1_lbl.TabIndex = 0;
+            this.num_1_lbl.Text = "1";
+            // 
+            // IsMultipliedBy
+            // 
+            this.IsMultipliedBy.AutoSize = true;
+            this.IsMultipliedBy.Font = new System.Drawing.Font("微软雅黑", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.IsMultipliedBy.Location = new System.Drawing.Point(157, 9);
+            this.IsMultipliedBy.Name = "IsMultipliedBy";
+            this.IsMultipliedBy.Size = new System.Drawing.Size(75, 75);
+            this.IsMultipliedBy.TabIndex = 1;
+            this.IsMultipliedBy.Text = "×";
+            // 
+            // num_2_lbl
+            // 
+            this.num_2_lbl.AutoSize = true;
+            this.num_2_lbl.Font = new System.Drawing.Font("微软雅黑", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.num_2_lbl.Location = new System.Drawing.Point(238, 9);
+            this.num_2_lbl.Name = "num_2_lbl";
+            this.num_2_lbl.Size = new System.Drawing.Size(67, 75);
+            this.num_2_lbl.TabIndex = 2;
+            this.num_2_lbl.Text = "1";
+            // 
+            // NewWork_btn
+            // 
+            this.NewWork_btn.Location = new System.Drawing.Point(12, 276);
+            this.NewWork_btn.Name = "NewWork_btn";
+            this.NewWork_btn.Size = new System.Drawing.Size(75, 23);
+            this.NewWork_btn.TabIndex = 5;
+            this.NewWork_btn.Text = "新题";
+            this.NewWork_btn.UseVisualStyleBackColor = true;
+            this.NewWork_btn.Click += new System.EventHandler(this.NewWork_btn_Click);
+            // 
+            // input_textbox
+            // 
+            this.input_textbox.Location = new System.Drawing.Point(112, 211);
+            this.input_textbox.Name = "input_textbox";
+            this.input_textbox.Size = new System.Drawing.Size(90, 21);
+            this.input_textbox.TabIndex = 6;
+            // 
+            // Submit_btn
+            // 
+            this.Submit_btn.Location = new System.Drawing.Point(205, 212);
+            this.Submit_btn.Name = "Submit_btn";
+            this.Submit_btn.Size = new System.Drawing.Size(69, 20);
+            this.Submit_btn.TabIndex = 7;
+            this.Submit_btn.Text = "提交";
+            this.Submit_btn.UseVisualStyleBackColor = true;
+            this.Submit_btn.Click += new System.EventHandler(this.Submit_btn_Click);
+            // 
+            // WorkNum_lbl
+            // 
+            this.WorkNum_lbl.AutoSize = true;
+            this.WorkNum_lbl.Location = new System.Drawing.Point(96, 281);
+            this.WorkNum_lbl.Name = "WorkNum_lbl";
+            this.WorkNum_lbl.Size = new System.Drawing.Size(41, 12);
+            this.WorkNum_lbl.TabIndex = 8;
+            this.WorkNum_lbl.Text = "题数 0";
+            // 
+            // one_btn
+            // 
+            this.one_btn.Location = new System.Drawing.Point(112, 107);
+            this.one_btn.Name = "one_btn";
+            this.one_btn.Size = new System.Drawing.Size(50, 20);
+            this.one_btn.TabIndex = 9;
+            this.one_btn.Text = "1";
+            this.one_btn.UseVisualStyleBackColor = true;
+            this.one_btn.Click += new System.EventHandler(this.one_btn_Click);
+            // 
+            // two_btn
+            // 
+            this.two_btn.Location = new System.Drawing.Point(168, 107);
+            this.two_btn.Name = "two_btn";
+            this.two_btn.Size = new System.Drawing.Size(50, 20);
+            this.two_btn.TabIndex = 10;
+            this.two_btn.Text = "2";
+            this.two_btn.UseVisualStyleBackColor = true;
+            this.two_btn.Click += new System.EventHandler(this.two_btn_Click);
+            // 
+            // three_btn
+            // 
+            this.three_btn.Location = new System.Drawing.Point(224, 107);
+            this.three_btn.Name = "three_btn";
+            this.three_btn.Size = new System.Drawing.Size(50, 20);
+            this.three_btn.TabIndex = 11;
+            this.three_btn.Text = "3";
+            this.three_btn.UseVisualStyleBackColor = true;
+            this.three_btn.Click += new System.EventHandler(this.three_btn_Click);
+            // 
+            // four_btn
+            // 
+            this.four_btn.Location = new System.Drawing.Point(112, 133);
+            this.four_btn.Name = "four_btn";
+            this.four_btn.Size = new System.Drawing.Size(50, 20);
+            this.four_btn.TabIndex = 12;
+            this.four_btn.Text = "4";
+            this.four_btn.UseVisualStyleBackColor = true;
+            this.four_btn.Click += new System.EventHandler(this.four_btn_Click);
+            // 
+            // five_btn
+            // 
+            this.five_btn.Location = new System.Drawing.Point(168, 133);
+            this.five_btn.Name = "five_btn";
+            this.five_btn.Size = new System.Drawing.Size(50, 20);
+            this.five_btn.TabIndex = 13;
+            this.five_btn.Text = "5";
+            this.five_btn.UseVisualStyleBackColor = true;
+            this.five_btn.Click += new System.EventHandler(this.five_btn_Click);
+            // 
+            // six_btn
+            // 
+            this.six_btn.Location = new System.Drawing.Point(224, 133);
+            this.six_btn.Name = "six_btn";
+            this.six_btn.Size = new System.Drawing.Size(50, 20);
+            this.six_btn.TabIndex = 14;
+            this.six_btn.Text = "6";
+            this.six_btn.UseVisualStyleBackColor = true;
+            this.six_btn.Click += new System.EventHandler(this.six_btn_Click);
+            // 
+            // seven_btn
+            // 
+            this.seven_btn.Location = new System.Drawing.Point(112, 159);
+            this.seven_btn.Name = "seven_btn";
+            this.seven_btn.Size = new System.Drawing.Size(50, 20);
+            this.seven_btn.TabIndex = 15;
+            this.seven_btn.Text = "7";
+            this.seven_btn.UseVisualStyleBackColor = true;
+            this.seven_btn.Click += new System.EventHandler(this.seven_btn_Click);
+            // 
+            // eight_btn
+            // 
+            this.eight_btn.Location = new System.Drawing.Point(168, 159);
+            this.eight_btn.Name = "eight_btn";
+            this.eight_btn.Size = new System.Drawing.Size(50, 20);
+            this.eight_btn.TabIndex = 16;
+            this.eight_btn.Text = "8";
+            this.eight_btn.UseVisualStyleBackColor = true;
+            this.eight_btn.Click += new System.EventHandler(this.eight_btn_Click);
+            // 
+            // nine_btn
+            // 
+            this.nine_btn.Location = new System.Drawing.Point(224, 159);
+            this.nine_btn.Name = "nine_btn";
+            this.nine_btn.Size = new System.Drawing.Size(50, 20);
+            this.nine_btn.TabIndex = 17;
+            this.nine_btn.Text = "9";
+            this.nine_btn.UseVisualStyleBackColor = true;
+            this.nine_btn.Click += new System.EventHandler(this.nine_btn_Click);
+            // 
+            // zero_btn
+            // 
+            this.zero_btn.Location = new System.Drawing.Point(112, 185);
+            this.zero_btn.Name = "zero_btn";
+            this.zero_btn.Size = new System.Drawing.Size(75, 20);
+            this.zero_btn.TabIndex = 18;
+            this.zero_btn.Text = "0";
+            this.zero_btn.UseVisualStyleBackColor = true;
+            this.zero_btn.Click += new System.EventHandler(this.zero_btn_Click);
+            // 
+            // Clear_btn
+            // 
+            this.Clear_btn.Location = new System.Drawing.Point(199, 185);
+            this.Clear_btn.Name = "Clear_btn";
+            this.Clear_btn.Size = new System.Drawing.Size(75, 20);
+            this.Clear_btn.TabIndex = 19;
+            this.Clear_btn.Text = "清空";
+            this.Clear_btn.UseVisualStyleBackColor = true;
+            this.Clear_btn.Click += new System.EventHandler(this.Clear_btn_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 311);
+            this.Controls.Add(this.Clear_btn);
+            this.Controls.Add(this.zero_btn);
+            this.Controls.Add(this.nine_btn);
+            this.Controls.Add(this.eight_btn);
+            this.Controls.Add(this.seven_btn);
+            this.Controls.Add(this.six_btn);
+            this.Controls.Add(this.five_btn);
+            this.Controls.Add(this.four_btn);
+            this.Controls.Add(this.three_btn);
+            this.Controls.Add(this.two_btn);
+            this.Controls.Add(this.one_btn);
+            this.Controls.Add(this.WorkNum_lbl);
+            this.Controls.Add(this.Submit_btn);
+            this.Controls.Add(this.input_textbox);
+            this.Controls.Add(this.NewWork_btn);
+            this.Controls.Add(this.num_2_lbl);
+            this.Controls.Add(this.IsMultipliedBy);
+            this.Controls.Add(this.num_1_lbl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "九九乘法口诀表练习器 — by Hurrieam";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label num_1_lbl;
+        private System.Windows.Forms.Label IsMultipliedBy;
+        private System.Windows.Forms.Label num_2_lbl;
+        private System.Windows.Forms.Button NewWork_btn;
+        private System.Windows.Forms.TextBox input_textbox;
+        private System.Windows.Forms.Button Submit_btn;
+        private System.Windows.Forms.Label WorkNum_lbl;
+        private System.Windows.Forms.Button one_btn;
+        private System.Windows.Forms.Button two_btn;
+        private System.Windows.Forms.Button three_btn;
+        private System.Windows.Forms.Button four_btn;
+        private System.Windows.Forms.Button five_btn;
+        private System.Windows.Forms.Button six_btn;
+        private System.Windows.Forms.Button seven_btn;
+        private System.Windows.Forms.Button eight_btn;
+        private System.Windows.Forms.Button nine_btn;
+        private System.Windows.Forms.Button zero_btn;
+        private System.Windows.Forms.Button Clear_btn;
+    }
+}
+
---
+++ MultiplicationRule/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiplicationRule
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //定义全局变量
+        Random rdm = new Random();
+        int num_1, num_2, result, input, WorkNum = 1;//乘数1，乘数2，结果，输入的结果，题号数
+
+        private void one_btn_Click(object sender, EventArgs e)
+        {
+            input_textbox.Text += "1";
+        }
+
+        private void two_btn_Click(object sender, EventArgs e)
+        {
+            input_textbox.Text += "2";
+        }
+
+        private void three_btn_Click(object sender, EventArgs e)
+        {
+            input_textbox.Text += "3";
+        }
+
+        private void four_btn_Click(object sender, EventArgs e)
+        {
+            input_textbox.Text += "4";
+        }
+
+        private void five_btn_Click(object sender, EventArgs e)
+        {
+            input_textbox.Text += "5";
+        }
+
+        private void six_btn_Click(object sender, EventArgs e)
+        {
+            input_textbox.Text += "6";
+        }
+        
+        private void seven_btn_Click(object sender, EventArgs e)
+        {
+            input_textbox.Text += "7";
+        }
+
+        private void eight_btn_Click(object sender, EventArgs e)
+        {
+            input_textbox.Text += "8";
+        }
+
+        private void nine_btn_Click(object sender, EventArgs e)
+        {
+            input_textbox.Text += "9";
+        }
+
+        private void zero_btn_Click(object sender, EventArgs e)
+        {
+            input_textbox.Text += "0";
+        }
+
+        private void Clear_btn_Click(object sender, EventArgs e)
+        {
+            input_textbox.Text = "";
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            //改变全局变量的值（生成第一组随机数）
+            num_1 = rdm.Next(1, 9);
+            num_2 = rdm.Next(1, 9);
+            result = num_1 * num_2;
+
+            //把int数字转成String显示出来
+            num_1_lbl.Text = num_1.ToString();
+            num_2_lbl.Text = num_2.ToString();
+
+            WorkNum_lbl.Text = "题数 " + WorkNum.ToString();
+
+        }
+
+        private void NewWork_btn_Click(object sender, EventArgs e)
+        {
+            //生成新的一组随机数
+            num_1 = rdm.Next(1, 9);
+            num_2 = rdm.Next(1, 9);
+            result = num_1 * num_2;
+
+            num_1_lbl.Text = num_1.ToString();
+            num_2_lbl.Text = num_2.ToString();
+        }
+
+        private void Submit_btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                input = int.Parse(input_textbox.Text);
+                if (input == result)
+                {
+                    MessageBox.Show("正确！","提示");
+
+                    //生成新的一组随机数
+                    num_1 = rdm.Next(1, 9);
+                    num_2 = rdm.Next(1, 9);
+                    result = num_1 * num_2;
+
+                    num_1_lbl.Text = num_1.ToString();
+                    num_2_lbl.Text = num_2.ToString();
+
+                    //题数加一再显示出来
+                    WorkNum++;
+                    WorkNum_lbl.Text = "题数 " + WorkNum.ToString();
+
+                    input_textbox.Text = "";
+                }
+                else if(input != result)//只有if(input != result)会发生错误
+                {
+                    MessageBox.Show("错误！\n\n"+num_1+"×"+num_2+"="+result,"提示");
+
+                    input_textbox.Text = "";
+                }
+            }
+            catch
+            {
+                MessageBox.Show("您的输入有误或为空，请检查核对后重新输入","提示");
+
+                input_textbox.Text = "";
+            }
+        }
+    }
+}
